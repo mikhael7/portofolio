49ee949deb112d8de29bb7a84e1e5e24ca72810c
mikhael <mikhaellion@gmail.com>
2023-01-21 11:56:45 +0700

    Updates

---
+++ _app/immutable/chunks/1-36c7da0c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-bf4d45ce.js";export{t as component};
---
+++ _app/immutable/chunks/2-74801342.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fa80eff4.js";export{t as component};
