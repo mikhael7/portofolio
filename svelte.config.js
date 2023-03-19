2263b79e2b26d3bef58dda163e47031cbfc18702
Mikhael <mikhaellion@gmail.com>
2023-03-19 16:48:03 +0700

    dev to main (#8)

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import preprocess from 'svelte-preprocess';
 // import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
