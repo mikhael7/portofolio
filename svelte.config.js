c82390ba514c4743a07c9842092249ff4f9c80d4
Mikhael <mikhaellion@gmail.com>
2023-03-19 19:11:15 +0700

    dev to main (#10)

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,21 +7,21 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
+		vitePreprocess()
+		// preprocess({
+		// postcss: true
+		// })
 	],
 
 	kit: {
 		adapter: adapter({
-			pages: 'build', // This tells in which folder to build the static files to
-			assets: 'build', // This tells in which folder to build the static files to
-			fallback: null
-		}),
-		paths: {
-			base: '/portofolio' // The github repository name
-		}
+			// pages: 'build', // This tells in which folder to build the static files to
+			// assets: 'build', // This tells in which folder to build the static files to
+			// fallback: null
+		})
+		// paths: {
+		// 	base: '/portofolio' // The github repository name
+		// }
 	}
 };
 
